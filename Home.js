c3247fe27d46caee31b77533efaba4b4e081ae55
Charbel Khoury <118617062+CharbelKhoury1@users.noreply.github.com>
2023-05-20 14:01:17 +0300

    Here are some changes

--- Home.js
+++ Home.js
@@ -43,3 +43,94 @@ function(){
 
 
 
+
+
+const slides = document.querySelectorAll(".slide");
+//This part is for enabling the arrows left and right of the keyboard.
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+      // Move to the next slide
+      const activeSlide = document.querySelector(".slide.active");
+      const nextSlide = activeSlide.nextElementSibling || slides[0];
+      activeSlide.classList.remove("active");
+      nextSlide.classList.add("active");
+    } else if (event.key === "ArrowLeft") {
+      // Move to the previous slide
+      const activeSlide = document.querySelector(".slide.active");
+      const prevSlide = activeSlide.previousElementSibling || slides[slides.length - 1];
+      activeSlide.classList.remove("active");
+      prevSlide.classList.add("active");
+    
+    }
+    
+    // Loop back to the first slide when reaching the end
+    const activeSlide = document.querySelector(".slide.active");
+    if (!activeSlide) {
+      slides[0].classList.add("active");
+    }
+  });
+
+
+
+
+// Get the slider and slide elements
+const slider = document.querySelector('.slider');
+// Set the active slide index to 0
+let activeSlide = 0;
+
+// Set the touch start and end coordinates
+let touchStartX = 0;
+let touchEndX = 0;
+
+// Add event listeners for touch start, move, and end
+slider.addEventListener('touchstart', handleTouchStart);
+slider.addEventListener('touchmove', handleTouchMove);
+slider.addEventListener('touchend', handleTouchEnd);
+
+// Handle touch start
+function handleTouchStart(event) {
+touchStartX = event.touches[0].clientX;
+}
+
+// Handle touch move
+function handleTouchMove(event) {
+touchEndX = event.touches[0].clientX;
+}
+
+// Handle touch end
+function handleTouchEnd() {
+// Determine the direction of the swipe
+const deltaX = touchEndX - touchStartX;
+if (deltaX > 0) {
+// Swipe to the right
+showSlide(activeSlide - 1);
+} else if (deltaX < 0) {
+// Swipe to the left
+showSlide(activeSlide + 1);
+}
+}
+
+// Show the specified slide
+function showSlide(index) {
+// Check if the index is out of bounds
+if (index < 0) {
+index = slides.length - 1;
+} else if (index >= slides.length) {
+index = 0;
+}
+
+// Remove the active class from the current slide
+slides[activeSlide].classList.remove('active');
+
+// Add the active class to the specified slide
+slides[index].classList.add('active');
+
+// Update the active slide index
+activeSlide = index;
+
+// Update the active dot
+updateActiveDot();
+}
+
+
+
