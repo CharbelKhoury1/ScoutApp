b09fad3ea1bc9ac458bc5016c01039d7017cab45
D-elissa <daou_elissa@hotmail.com>
2023-06-09 15:21:18 +0300

    changes

--- views/javascript/transaction.js
+++ views/javascript/transaction.js
@@ -1,62 +1,83 @@
-let index = document.getElementById('table-body').rows.length;
-  
-    // Function to add a new row
-    function addRow() {
-      const tableBody = document.getElementById("table-body");
-      const newRow = document.createElement("tr");
-  
-      const indexCell = document.createElement("td");
-      index++;
-      indexCell.textContent = index;
-      newRow.appendChild(indexCell);
-  
-      const descriptionCell = document.createElement("td");
-      const descriptionInput = document.createElement("input");
-      descriptionInput.type = "text";
-      descriptionInput.name = "description[]";
-      descriptionInput.style.border = "none";
-      descriptionCell.appendChild(descriptionInput);
-      newRow.appendChild(descriptionCell);
-  
-      const lbpCell = document.createElement("td");
-      const lbpInput = document.createElement("input");
-      lbpInput.type = "text";
-      lbpInput.name = "lbp[]";
-      lbpInput.style.border = "none";
-      lbpCell.appendChild(lbpInput);
-      newRow.appendChild(lbpCell);
-  
-      const removeCell = document.createElement("td");
-      const removeIcon = document.createElement("i");
-      removeIcon.className = "fas fa-times remove-icon";
-      removeIcon.addEventListener("click", function () {
-        removeRow(this);
+function showOptions(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function hideOptions(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+function fetchBalance() {
+  fetch('../controllers/balanceController.php')
+      .then(response => response.json())
+      .then(data => {
+          const balanceTextLBP = document.createElement('div');
+          balanceTextLBP.innerHTML = `<h3 class="balance-text">Current Balance: ${data.lbp_balance} LBP</h3>`;
+          document.getElementById('chartContainerLBP').insertBefore(balanceTextLBP, document.getElementById('chartLBP'));
+
+          const balanceTextUSD = document.createElement('div');
+          balanceTextUSD.innerHTML = `<h3 class="balance-text">Current Balance: ${data.usd_balance} USD</h3>`;
+          document.getElementById('chartContainerUSD').insertBefore(balanceTextUSD, document.getElementById('chartUSD'));
+
+          const chartDataLBP = {
+              labels: ['LBP Income', 'LBP Expense'],
+              datasets: [{
+                  data: [data.lbp_income, data.lbp_expense],
+                  backgroundColor: ['#007bff', '#dc3545'],
+              }]
+          };
+
+          const chartContainerLBP = document.getElementById('chartContainerLBP');
+          chartContainerLBP.style.display = 'block'; // Display the LBP chart container
+
+          const chartLBP = document.getElementById('chartLBP').getContext('2d');
+          new Chart(chartLBP, {
+              type: 'doughnut',
+              data: chartDataLBP,
+              options: {
+                  responsive: true,
+                  maintainAspectRatio: false,
+                  legend: {
+                      position: 'bottom'
+                  },
+                  plugins: {
+                      title: {
+                          display: true,
+                          text: 'LBP Income and Expenses'
+                      }
+                  }
+              }
+          });
+
+          const chartDataUSD = {
+              labels: ['USD Income', 'USD Expense'],
+              datasets: [{
+                  data: [data.usd_income, data.usd_expense],
+                  backgroundColor: ['#007bff', '#dc3545'],
+              }]
+          };
+
+          const chartContainerUSD = document.getElementById('chartContainerUSD');
+          chartContainerUSD.style.display = 'block'; // Display the USD chart container
+
+          const chartUSD = document.getElementById('chartUSD').getContext('2d');
+          new Chart(chartUSD, {
+              type: 'doughnut',
+              data: chartDataUSD,
+              options: {
+                  responsive: true,
+                  maintainAspectRatio: false,
+                  legend: {
+                      position: 'bottom'
+                  },
+                  plugins: {
+                      title: {
+                          display: true,
+                          text: 'USD Income and Expenses'
+                      }
+                  }
+              }
+          });
+
+         
       });
-      removeCell.appendChild(removeIcon);
-      newRow.appendChild(removeCell);
-  
-      // Remove the additional column if it exists
-      const additionalCell = newRow.querySelector('.additional-column');
-      if (additionalCell) {
-        newRow.removeChild(additionalCell);
-      }
-  
-      tableBody.appendChild(newRow);
-    }
-  
-    // Function to remove a row
-    function removeRow(row) {
-      var tableBody = document.getElementById('table-body');
-      var currentRow = row.parentNode.parentNode;
-      tableBody.removeChild(currentRow);
-      index--;
-      updateIndex();
-    }
-  
-    // Function to update the index values
-    function updateIndex() {
-      var rows = document.getElementById('table-body').rows;
-      for (var i = 0; i < rows.length; i++) {
-        rows[i].cells[0].textContent = i + 1;
-      }
-    }
+}
--- views/transactionView.php
+++ views/transactionView.php
@@ -11,26 +11,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
 
     <title>Transaction View</title>
-    <style>
-    .container1 {
-        margin-top: 90px;
-        display: flex;
-        justify-content: center;
-    }
-
-    .chart-container {
-        width: 30%; /* Adjust the width as needed */
-        height: 300px;
-        margin: 0 -10px; /* Adjust the margin as needed */
-    }
-
-    .balance-text {
-    margin-left: 25%;
-    }
-
-</style>
-
-
 </head>
 <body>
     <?php require(__DIR__ . "/sideBar.php"); ?>
@@ -82,92 +62,7 @@
             <canvas id="chartUSD"></canvas>
         </div>
     </div>
-
-    <script>
-        function showOptions(id) {
-            document.getElementById(id).style.display = 'block';
-        }
-
-        function hideOptions(id) {
-            document.getElementById(id).style.display = 'none';
-        }
-
-        function fetchBalance() {
-            fetch('../controllers/balanceController.php')
-                .then(response => response.json())
-                .then(data => {
-                    const balanceTextLBP = document.createElement('div');
-                    balanceTextLBP.innerHTML = `<h3 class="balance-text">Current Balance: ${data.lbp_balance} LBP</h3>`;
-                    document.getElementById('chartContainerLBP').insertBefore(balanceTextLBP, document.getElementById('chartLBP'));
-
-                    const balanceTextUSD = document.createElement('div');
-                    balanceTextUSD.innerHTML = `<h3 class="balance-text">Current Balance: ${data.usd_balance} USD</h3>`;
-                    document.getElementById('chartContainerUSD').insertBefore(balanceTextUSD, document.getElementById('chartUSD'));
-
-                    const chartDataLBP = {
-                        labels: ['LBP Income', 'LBP Expense'],
-                        datasets: [{
-                            data: [data.lbp_income, data.lbp_expense],
-                            backgroundColor: ['#007bff', '#dc3545'],
-                        }]
-                    };
-
-                    const chartContainerLBP = document.getElementById('chartContainerLBP');
-                    chartContainerLBP.style.display = 'block'; // Display the LBP chart container
-
-                    const chartLBP = document.getElementById('chartLBP').getContext('2d');
-                    new Chart(chartLBP, {
-                        type: 'doughnut',
-                        data: chartDataLBP,
-                        options: {
-                            responsive: true,
-                            maintainAspectRatio: false,
-                            legend: {
-                                position: 'bottom'
-                            },
-                            plugins: {
-                                title: {
-                                    display: true,
-                                    text: 'LBP Income and Expenses'
-                                }
-                            }
-                        }
-                    });
-
-                    const chartDataUSD = {
-                        labels: ['USD Income', 'USD Expense'],
-                        datasets: [{
-                            data: [data.usd_income, data.usd_expense],
-                            backgroundColor: ['#007bff', '#dc3545'],
-                        }]
-                    };
-
-                    const chartContainerUSD = document.getElementById('chartContainerUSD');
-                    chartContainerUSD.style.display = 'block'; // Display the USD chart container
-
-                    const chartUSD = document.getElementById('chartUSD').getContext('2d');
-                    new Chart(chartUSD, {
-                        type: 'doughnut',
-                        data: chartDataUSD,
-                        options: {
-                            responsive: true,
-                            maintainAspectRatio: false,
-                            legend: {
-                                position: 'bottom'
-                            },
-                            plugins: {
-                                title: {
-                                    display: true,
-                                    text: 'USD Income and Expenses'
-                                }
-                            }
-                        }
-                    });
-
-                   
-                });
-        }
-    </script>
+    <script src="../views/javascript/transaction.js"></script>
 </body>
 </html>
 
