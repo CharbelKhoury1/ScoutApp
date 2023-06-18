f14df0be39752e7085fc8508b33bf1df0570f13a
Charbel Khoury <118617062+CharbelKhoury1@users.noreply.github.com>
2023-06-18 13:15:56 +0300

    login reset pass

--- Login/Login.php
+++ Login/Login.php
@@ -30,7 +30,7 @@ include "LoginCredentials.php";
             <input type="submit" id="submit-btn" name="submit-btn" value="Continue">
         
           </form>
-        <p class="register"><a href="#" > Forgot Password?</a></p>
+        <p class="register"><a href="../views/resetPassView.php" > Forgot Password?</a></p>
     </div>
 </body>
 </html>
