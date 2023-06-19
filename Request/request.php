76d35d7be4faf482d9155fcc09224c5e9dd571d6
PaulOjeil <89648217+PaulOjeil@users.noreply.github.com>
2023-06-19 20:00:37 +0300

    changes in request page

--- Request/request.php
+++ Request/request.php
@@ -1,94 +1,123 @@
 <?php
 
 session_start();
+
 include ("../common.inc.php");
 include ("../utility.php");
 $conn=connection();
 
+
+function calculateDaysDifference($d) {
+  $sysDate = date('Y-m-d'); // Get the current system date in 'Y-m-d' format
+
+  // Convert the selected date and system date to Unix timestamps
+  $selectedTimestamp = strtotime($d);
+  $sysTimestamp = strtotime($sysDate);
+
+  // Calculate the difference in seconds
+  $difference = $selectedTimestamp - $sysTimestamp;
+
+  // Convert the difference to days
+  $daysDifference = floor($difference / (60 * 60 * 24));
+
+  return $daysDifference;
+}
+
+
 if (isset($_POST['submit'])) {
-  if (isset($_POST['dateN']) && isset($_POST['receiverN']) && isset($_POST['descriptionN']) && isset($_FILES['file'])) {
+  if (!empty($_POST['dateN']) && !empty($_POST['receiverN']) && !empty($_POST['descriptionN']) && !empty($_FILES['file'])) {
     $d = $_POST['dateN'];
     $rec = $_POST['receiverN'];
     $desc = $_POST['descriptionN'];
     $file = $_FILES['file'];
 
-    $fileName = $file['name'];
-    $fileTmpName = $_FILES['file']['tmp_name'];
+    $days = calculateDaysDifference($d);
+    if($days >= 14){
 
-    // Read file data
-    $fileData = file_get_contents($fileTmpName);
+      $fileName = $file['name'];
+      $fileTmpName = $_FILES['file']['tmp_name'];
 
-    $f = explode('.', $fileName);
-    $fileExt = strtolower($f[1]);
+      // Read file data
+      $fileData = file_get_contents($fileTmpName);
 
-    $allowedExt = array('pdf');
-    if (in_array($fileExt, $allowedExt)) {
-      $sql = "INSERT INTO `requests` (`date_submitted`, `date_of_event`, `approver`, `description`, `name`, `data`) VALUES (SYSDATE(), ?, ?, ?, ?, ?)";
-      $stmt = mysqli_prepare($conn, $sql);
+      $f = explode('.', $fileName);
+      $fileExt = strtolower($f[1]);
+
+      $allowedExt = array('pdf');
+      if (in_array($fileExt, $allowedExt)) {
+        $sql = "INSERT INTO `requests` (`date_submitted`, `date_of_event`, `approver`, `description`, `name`, `data`) VALUES (SYSDATE(), ?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($conn, $sql);
       
-      // Bind the parameters
-      mysqli_stmt_bind_param($stmt, 'sssss', $d, $rec, $desc, $fileName, $fileData);
+        // Bind the parameters
+        mysqli_stmt_bind_param($stmt, 'sssss', $d, $rec, $desc, $fileName, $fileData);
       
-      // Execute the statement
-      $result = mysqli_stmt_execute($stmt);
+        // Execute the statement
+        $result = mysqli_stmt_execute($stmt);
       
-      if ($result) {
-        header("Location: process.html?");
+        if ($result) {
+        //header("Location: process.html?");
 
-        $qr4 = "SELECT max(request_id) FROM requests";
-        $res4 = mysqli_query($conn, $qr4);
-        $row4 = mysqli_fetch_array($res4);
-        $idd = $row4[0];
+          $qr4 = "SELECT max(request_id) FROM requests";
+          $res4 = mysqli_query($conn, $qr4);
+          $row4 = mysqli_fetch_array($res4);
+          $idd = $row4[0];
 
-        $qr5 = "INSERT INTO `requeststatus`(`date`, `statusCode`, `request_id`, `userId`) VALUES (SYSDATE() , '0' , '$idd' , '1')";
-        $res5 = mysqli_query($conn, $qr5);
+          $qr5 = "INSERT INTO `requeststatus`(`date`, `statusCode`, `request_id` , `userId` ) VALUES (SYSDATE() , '0' , '$idd' , '15')";
+          $res5 = mysqli_query($conn, $qr5);
 
-        if (isset($_FILES['mediaFile']) || isset($_POST['caption'])) {
-          $cap = $_POST['caption'];
-          $mediaFile = $_FILES['mediaFile'];
+          if (isset($_FILES['mediaFile']) || isset($_POST['caption'])) {
 
-          $fileN = $mediaFile['name'];
-          $fileTmpLocat = $mediaFile['tmp_name'];
+            $cap = $_POST['caption'];
+            $mediaFile = $_FILES['mediaFile'];
 
-          $fM = explode('.', $fileN);
-          $fileExtension = strtolower($fM[1]);
+            $fileN = $mediaFile['name'];
+            $fileTmpLocat = $mediaFile['tmp_name'];
 
-          $allowedExtension = array('jpeg', 'jpg', 'png');
-          if (in_array($fileExtension, $allowedExtension)) {
-            $qr1 = "SELECT max(request_id) FROM requests";
-            $res1 = mysqli_query($conn, $qr1);
-            $row1 = mysqli_fetch_array($res1);
-            $id = $row1[0];
+            $fM = explode('.', $fileN);
+            $fileExtension = strtolower($fM[1]);
 
-            $qr2 = "SELECT date_of_event FROM requests WHERE request_id=$id";
-            $res2 = mysqli_query($conn, $qr2);
-            $row2 = mysqli_fetch_array($res2);
-            $doe = $row2[0];
+            $allowedExtension = array('jpeg', 'jpg', 'png');
+            if (in_array($fileExtension, $allowedExtension)) {
+              $qr1 = "SELECT max(request_id) FROM requests";
+              $res1 = mysqli_query($conn, $qr1);
+              $row1 = mysqli_fetch_array($res1);
+              $id = $row1[0];
 
-            $qr3 = "INSERT INTO `event`(`description`, `date_of_event`, `request_id`) VALUES ('$cap' , '$doe' , '$id')";
-            $res3 = mysqli_query($conn, $qr3);
+              $qr2 = "SELECT date_of_event FROM requests WHERE request_id=$id";
+              $res2 = mysqli_query($conn, $qr2);
+              $row2 = mysqli_fetch_array($res2);
+              $doe = $row2[0];
 
-            if ($res3) {
-              header("Location: process.html?");
-            } else {
-              header("Location: request.php?error=An error occurred. Not Added!");
+              $qr3 = "INSERT INTO `event`(`description`, `date_of_event`, `request_id`) VALUES ('$cap' , '$doe' , '$id')";
+              $res3 = mysqli_query($conn, $qr3);
+
+              if ($res3) {
+                header("Location: process.html?");
+              } else {
+                header("Location: requestIndex.php?error=An error occurred. res3!");
+              }
+            } else{
+              header("Location: requestIndex.php?error=File not supported!");
             }
           }else{
-            header("Location: request.php?error=File not supported!");
+            header("Location: requestIndex.php?error=All fields are required!");
           }
         }else{
-          header("Location: request.php?error=All fields are required!");
+          header("Location: requestIndex.php?error=An error! result");
         }
-      }else{
-        header("Location: request.php?error=An error occurred!");
+      } else {
+        header("Location: requestIndex.php?error=File error supported!");
       }
-    } else {
-      header("Location: request.php?error=File not supported!");
+    }else {
+      header("Location: requestIndex.php?error=Event date must be 14 days apart from today's date!");
     }
+  //header("Location: process.html?");
   } else {
-    header("Location: request.php?error=All fields are required!");
+    header("Location: requestIndex.php?error=All fields are required! big");
   }
+  //header("Location: process.html?");
 }
+<<<<<<< Updated upstream
 ?>
 
 <!DOCTYPE html>
@@ -204,3 +233,6 @@ if (isset($_POST['submit'])) {
 
 
 </html>
+=======
+?>
+>>>>>>> Stashed changes
