b49a95b65d1f3b9c1e19734428501bc0a39d533f
D-elissa <daou_elissa@hotmail.com>
2023-06-26 16:25:40 +0300

    changes in transactions

--- controllers/viewController.php
+++ controllers/viewController.php
@@ -3,28 +3,28 @@ include("../models/balanceTrialModel.php");
 
 if (isset($_GET['transaction_id'])){
     $transactionId = $_GET['transaction_id'];
-        $con = connection();
-        $query = "SELECT attachment FROM `transaction` WHERE transaction_id = ? AND attachment IS NOT NULL";
-        $stmt = mysqli_prepare($con, $query);
-        mysqli_stmt_bind_param($stmt, "i", $transactionId);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        $row = mysqli_fetch_assoc($result);
+    $con = connection();
+    $query = "SELECT attachment FROM `transaction` WHERE transaction_id = ? AND attachment IS NOT NULL";
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "i", $transactionId);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($result);
 
-        if ($row) {
-            $fileData = $row['attachment'];
-            $fileExtension = pathinfo($row['attachment'], PATHINFO_EXTENSION);
-            $contentType = 'application/pdf';
+    if ($row) {
+        $fileData = $row['attachment'];
+        $fileExtension = pathinfo($row['attachment'], PATHINFO_EXTENSION);
+        $contentType = 'application/pdf';
 
-            header('Content-Type: application/pdf');
-            header('Content-Disposition: inline; filename="transaction"');
-            header('Content-Length: ' . strlen($fileData));
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: inline; filename="transaction"');
+        header('Content-Length: ' . strlen($fileData));
 
-            // Output the PDF data
-            ob_clean();
-            flush();
-            echo $fileData;
-            exit();
-        } 
+        // Output the PDF data
+        ob_clean();
+        flush();
+        echo $fileData;
+        exit();
+    } 
 }
 ?>
--- views/javascript/balanceTrial.js
+++ views/javascript/balanceTrial.js
@@ -1,37 +1,33 @@
-let currentData = {}; // Store the current data globally
+let currentData = { transactionRecords: [] }; // Store the current data globally
 
 // Function to handle form submission
 function handleFormSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const form = event.target;
-  const formData = new FormData(form);
+    const form = event.target;
+    const formData = new FormData(form);
 
-  const currencyCode = formData.get('currency-code');
-  const typeCode = formData.get('type-code');
+    const currencyCode = formData.get('currency-code');
+    const typeCode = formData.get('type-code');
 
-  // Make AJAX request to the controller endpoint
-  fetch('../controllers/balanceTrialController.php', {
-    method: 'POST',
-    body: new URLSearchParams({
-      currency_code: currencyCode,
-      type_code: typeCode
+    // Make AJAX request to the controller endpoint
+    fetch('../controllers/balanceTrialController.php', {
+          method: 'POST',
+          body: new URLSearchParams({
+                'currency_code': currencyCode,
+                'type_code': typeCode
+          })
     })
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Update the current data with the new response
-      currentData = data;
-
-      // Display the transaction records in the table
-      displayTransactionRecords();
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+          currentData = data;
+          displayTransactionRecords();
+      })
+        .catch(error => {
+          console.error('Error:', error);
+        });
 }
 
-// Function to display transaction records in the table
 function displayTransactionRecords() {
   const tableContainer = document.getElementById('transaction-table');
   tableContainer.innerHTML = '';
@@ -60,20 +56,76 @@ function displayTransactionRecords() {
   // Create table header row
   const headerRow = document.createElement('tr');
   for (const key in currentData.transactionRecords[0]) {
-    const th = document.createElement('th');
-    th.textContent = key;
-    headerRow.appendChild(th);
+    if (key !== 'transaction_id' && key !== 'attachment') {
+      // Exclude the 'transaction_id' and 'attachment' columns
+      const th = document.createElement('th');
+      th.textContent = key;
+      headerRow.appendChild(th);
+    }
   }
+
+  const attachmentHeader = document.createElement('th');
+  attachmentHeader.textContent = 'Attachment';
+  headerRow.appendChild(attachmentHeader);
+
+  const actionsHeader = document.createElement('th');
+  actionsHeader.textContent = 'Actions';
+  headerRow.appendChild(actionsHeader);
+
   tableHeader.appendChild(headerRow);
 
   // Create table rows for each record
   currentData.transactionRecords.forEach(record => {
     const row = document.createElement('tr');
+    row.dataset.transactionId = record.transaction_id; // Store transaction ID in data attribute
+
     for (const key in record) {
-      const td = document.createElement('td');
-      td.textContent = record[key];
-      row.appendChild(td);
+      if (key !== 'transaction_id' && key !== 'attachment') {
+        // Exclude the 'transaction_id' and 'attachment' columns
+        const td = document.createElement('td');
+        td.textContent = record[key];
+        row.appendChild(td);
+      }
     }
+
+    const attachmentCell = document.createElement('td');
+    if (record.attachment) {
+      const viewButton = document.createElement('button');
+      viewButton.textContent = 'View';
+      viewButton.dataset.transactionId = record.transaction_id; // Store transaction ID in data attribute
+      viewButton.dataset.action = 'view';
+      viewButton.addEventListener('click', handleActionButtonClick);
+
+      const downloadButton = document.createElement('button');
+      downloadButton.textContent = 'Download';
+      downloadButton.dataset.transactionId = record.transaction_id; // Store transaction ID in data attribute
+      downloadButton.dataset.action = 'download';
+      downloadButton.addEventListener('click', handleActionButtonClick);
+
+      attachmentCell.appendChild(viewButton);
+      attachmentCell.appendChild(downloadButton);
+    } else {
+      attachmentCell.textContent = 'Not available';
+    }
+
+    const actionsCell = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.dataset.transactionId = record.transaction_id; // Store transaction ID in data attribute
+    deleteButton.dataset.action = 'delete';
+    deleteButton.addEventListener('click', handleActionButtonClick);
+
+    const updateButton = document.createElement('button');
+    updateButton.textContent = 'Update';
+    updateButton.dataset.transactionId = record.transaction_id; // Store transaction ID in data attribute
+    updateButton.dataset.action = 'update';
+    updateButton.addEventListener('click', handleActionButtonClick);
+
+    actionsCell.appendChild(deleteButton);
+    actionsCell.appendChild(updateButton);
+
+    row.appendChild(attachmentCell);
+    row.appendChild(actionsCell);
     tableBody.appendChild(row);
   });
 
@@ -81,13 +133,34 @@ function displayTransactionRecords() {
   table.appendChild(tableBody);
   tableContainer.innerHTML = ''; // Clear the table container
   tableContainer.appendChild(currencyTypeElement); // Append the currency and type element
-  tableContainer.appendChild(table); // Append the new table
-
-  // Reset the form
-  const balanceForm = document.getElementById('balance-form');
-  balanceForm.reset();
+  tableContainer.appendChild(table); // Append the new table to the container
 }
 
-// Add event listener to the form submit event
+
+// Function to handle delete and update button clicks
+function handleActionButtonClick(event) {
+  const button = event.target;
+  const transactionId = button.dataset.transactionId;
+  const action = button.dataset.action;
+
+  if (action === 'delete') {
+      const confirmDelete = confirm('Are you sure you want to delete this transaction?');
+      if (!confirmDelete) {
+            return;
+      }
+                
+        window.location.href = '../controllers/deleteTransactionController.php?transaction_id=' + transactionId;
+      } else if (action === 'update') {
+         window.location.href = `../controllers/updateTransactionController.php?transaction_id=${transactionId}`;
+      } else if (action === 'view') {
+          window.location.href = `../controllers/viewController.php?transaction_id=${transactionId}`;
+      } else if (action === 'download') {
+          window.location.href = `../controllers/downloadController.php?transaction_id=${transactionId}`;
+   }
+}
+
+ // Attach form submission event listener
 const balanceForm = document.getElementById('balance-form');
 balanceForm.addEventListener('submit', handleFormSubmit);
+
+
